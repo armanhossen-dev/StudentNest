0660dc689088f6827dd46a72641b55cc1a46c3fe
Arman Hossen <2armanhossen@gmail.com>
2025-08-06 05:32:22 +0600

    v1.4

--- JAVA PROJECT/StudentNest/src/main/java/module-info.java
+++ JAVA PROJECT/StudentNest/src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module com.studentnest.studentnest {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+//    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires net.synedra.validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+//    requires com.almasb.fxgl.all;
 
     opens com.studentnest to javafx.fxml;
     exports com.studentnest;
